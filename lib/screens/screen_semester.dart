2fe9d36e9d0bc54c8af339098ddad3a496c12b6c
Shahar Raz <SRazstudent@gmail.com>
2020-10-14 19:12:27 +0300

    finished basic logic. works for a simple single view, now implementing course remove (start from the course->semester->year->degree)

--- lib/screens/screen_semester.dart
+++ lib/screens/screen_semester.dart
@@ -9,79 +9,83 @@ import 'package:gpa_israel/logic/academic_semester.dart';
 class SemesterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
-      floatingActionButton: FloatingActionButton(
+    return Consumer<LogicManager>(
+      // consumer vid 220, 15:00 flutter Udemy Angela
+      builder: (context, degreeData, child) {
+        return Scaffold(
           backgroundColor: Colors.lightBlueAccent,
-          child: Icon(Icons.add),
-          onPressed: () {
-            showModalBottomSheet(
-                context: context,
-                isScrollControlled: true,
-                builder: (context) => SingleChildScrollView(
-                        child: Container(
-                      padding: EdgeInsets.only(
-                          bottom: MediaQuery.of(context).viewInsets.bottom),
-                      child: AddCourseScreen(),
-                    )));
-          }),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(
-                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                CircleAvatar(
-                  child: Icon(
-                    Icons.list,
-                    size: 30.0,
-                    color: Colors.lightBlueAccent,
-                  ),
-                  backgroundColor: Colors.white,
-                  radius: 30.0,
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  'GPA  ${Provider.of<LogicManager>(context).getSemester(yearTaken_1to7: 1, semesterTaken_1to3: 1).getSemesterAverage()}',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 50.0,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                Text(
-                  //'${Provider.of<LogicManager>(context).getCourseCount} Tasks',
-                  '${Provider.of<LogicManager>(context).getCourseCount(yearTaken_1to7: 1, semesterTaken_1to3: 1)} Courses',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  topRight: Radius.circular(20.0),
+          floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.lightBlueAccent,
+              child: Icon(Icons.add),
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    isScrollControlled: true,
+                    builder: (context) => SingleChildScrollView(
+                            child: Container(
+                          padding: EdgeInsets.only(
+                              bottom: MediaQuery.of(context).viewInsets.bottom),
+                          child: AddCourseScreen(),
+                        )));
+              }),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(
+                    top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    CircleAvatar(
+                      child: Icon(
+                        Icons.list,
+                        size: 30.0,
+                        color: Colors.lightBlueAccent,
+                      ),
+                      backgroundColor: Colors.white,
+                      radius: 30.0,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      'GPA  ${degreeData.getSemester(yearTaken_1to7: 1, semesterTaken_1to3: 1).getSemesterAverage()}',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text(
+                      //'${Provider.of<LogicManager>(context).getCourseCount} Tasks',
+                      '${degreeData.getCourseCount(yearTaken_1to7: 1, semesterTaken_1to3: 1)} Courses',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              child: Widget_SemesterCoursesList(
-                  Provider.of<LogicManager>(context)
-                      .getSemester(yearTaken_1to7: 1, semesterTaken_1to3: 1)),
-            ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    ),
+                  ),
+                  child: Widget_SemesterCoursesList(degreeData.getSemester(
+                      yearTaken_1to7: 1, semesterTaken_1to3: 1)),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
